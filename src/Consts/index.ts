c32fcfe2ae1c12d1a12cff38cbbe558c67782117
ttzztztz <935319340@qq.com>
2019-07-11 23:29:38 +0800

    Update dependencies

--- src/Consts/index.ts
+++ src/Consts/index.ts
@@ -1,5 +1,5 @@
-export const VERSION = "2.0.0";
-export const BUILD_DATE = "20190607";
+export const VERSION = "1.0.0";
+export const BUILD_DATE = "20190709";
 
 export const ICP_BEIAN = "冀ICP备18020890号";
 export const GONGAN_BEIAN = "冀公网安备13030402000133号";
