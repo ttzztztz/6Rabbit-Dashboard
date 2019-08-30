9787f2a4fecd25618b26f7ca98c049d1527a3a93
ttzztztz <935319340@qq.com>
2019-08-30 14:04:34 +0800

    Fix router

--- consts/routers.ts
+++ consts/routers.ts
@@ -8,7 +8,7 @@ export const ADMIN = `/admin`;
 
 export const USER_PROFILE_RAW = `/profile/[uid]`;
 export const THREAD_INFO_RAW = `/thread/[tid]/[page]`;
-export const THREAD_UPDATE_RAW = `/thread/update/[pid]`;
+export const THREAD_UPDATE_RAW = `/thread/update/[tid]`;
 export const THREAD_LIST_RAW = `/forum/[fid]/[page]`;
 export const POST_UPDATE_RAW = `/post/update/[pid]`;
 export const SEARCH_RAW = `/search/[keywords]/[page]`;
@@ -18,7 +18,7 @@ export const THREAD_CREATE = `/thread/create`;
 export const USER_PROFILE = (uid: string) => `/profile/${uid}`;
 export const THREAD_INFO = (tid: string, page: string = "1") => `/thread/${tid}/${page}`;
 export const THREAD_LIST = (fid: string, page: string = "1") => `/forum/${fid}/${page}`;
-export const THREAD_UPDATE = (pid: string) => `/thread/update/${pid}`;
+export const THREAD_UPDATE = (tid: string) => `/thread/update/${tid}`;
 export const THREAD_REPLY = (tid: string) => `/thread/reply/${tid}`;
 export const POST_UPDATE = (pid: string) => `/post/update/${pid}`;
 export const SEARCH = (keywords: string, page: string) => `/search/${keywords}/${page}`;
